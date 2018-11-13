911ffb2bfdffa350e45a00044cb589459103c6eb
Simon King <simon.king@stronans.com>
2018-11-13 22:57:07 +0000

    Changed port and added error handler to Spring app to redirect any unknown pages to the Angular app.

--- hydrogui/src/app/services/server.services.ts
+++ hydrogui/src/app/services/server.services.ts
@@ -9,14 +9,14 @@ import {HttpClient} from "@angular/common/http";
 export class DataServerService {
 
   public Host:string = window.location.hostname;
-  public Port:string = window.location.port;
+  public Port:string = window.location.port; // Will be 4200 in this case.
   public Server:string = "http://" + this.Host + ":" + this.Port;
 
   constructor(private _http: HttpClient) {
   }
 
   public executeOption = (url: string): Observable<string> => {
-    console.log("url name = ", this.Server + url);
+    console.log("url name = [" + this.Server + url + "]");
     return this._http.get<string>(this.Server + url);
   };
 }
